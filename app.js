0033ade7a343bcf145a7185ccef42984ecd27f63
spiderforrest <spider@spood.org>
2022-11-07 15:19:54 -0800

    got friend list displaying/interacting working, added lib

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // import functions and grab DOM elements
 import { renderMushroom, renderFriend } from './lib/render-utils.js';
 import { getRandName } from './lib/get-name.js';
+import { findFriendByName } from './lib/data-utils.js';
 
 const friendsEl = document.querySelector('.friends');
 const friendInputEl = document.getElementById('friend-input');
 const mushroomsEl = document.querySelector('.mushrooms');
 const addMushroomButton = document.getElementById('add-mushroom-button');
 const addFriendButton = document.getElementById('add-friend-button');
+const friendsDiv = document.getElementById('friends-div');
 // initialize state
 
 let mushroomCount = 3;
@@ -55,21 +57,37 @@ addFriendButton.addEventListener('click', () => {
     friendInputEl.value = '';
     // display all the friends (use a function here)
     displayFriends();
-    console.log(friendData);
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsDiv.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
-        //         and if the friend's satisfaction level is below 3 and you have mushrooms left
-        //             increment the friends satisfaction and decrement your mushrooms
-        //             then display your friends and mushrooms with the updated state
+        friendEl.addEventListener('click', () => {
+            const friendInState = findFriendByName(friend.name, friendData);
+            // and if the friend's satisfaction level is below 3 and you have mushrooms left
+            if (mushroomCount > 0 && friendInState.satisfaction < 3) {
+                // increment the friends satisfaction and decrement your mushrooms
+                friendInState.satisfaction++;
+                mushroomCount--;
+                // then display your friends and mushrooms with the updated state
+                displayFriends();
+                displayMushrooms();
+            }
+            // tell the user what's wrong
+            if (mushroomCount === 0) {
+                alert('Out of mushrooms!');
+            } else {
+                alert('Friend is full!');
+            }
+        });
         // append the friendEl to the friends list in DOM
+        friendsEl.append(friendEl);
     }
 }
 
